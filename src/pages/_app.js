a52711a352378fa37332cbd3e42c7160ae991259
alabobriggs <alabobriggs@gmail.com>
2020-03-31 10:56:25 +0100

    style(style.cw): move styles to root to attempt style fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
-import '../styles/styles.css';
+import '../../styles.css';
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
